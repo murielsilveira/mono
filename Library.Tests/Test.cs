147744f3b02f1df02c87348d4c89a6f4c309e63c
Muriel Silveira <murielkong@gmail.com>
2015-11-14 18:23:01 -0200

    Add a test with SQLite

---
+++ Library.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Library.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            new Database().Conectar();
+        }
+    }
+}
+
---
+++ Library/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Mono.Data.Sqlite;
+
+namespace Library
+{
+    public class Database
+    {
+        public Database()
+        {
+            
+        }
+
+        public void Conectar()
+        {
+            using (var conn = new SqliteConnection("URI=file:/home/muriel/dev/csharp/Mono/muriel.db3"))
+            {
+                conn.Open();
+                conn.Clone();
+            }
+        }
+    }
+}
+
