5f593ea87afcd2c371d081956aac612b7433a8b4
Muriel Silveira <murielkong@gmail.com>
2015-11-24 17:54:43 -0200

    Utilizar métodos que estão falhando no build no appveyor.

--- Library/Database.cs
+++ Library/Database.cs
@@ -5,6 +5,8 @@ namespace Library
 {
     public class Database
     {
+        string ErroBD;
+
         public Database()
         {
             
@@ -13,8 +15,26 @@ namespace Library
         public void Conectar()
         {
             string path = AppDomain.CurrentDomain.BaseDirectory + "muriel.db3";
+
+            this.ErroBD = string.Empty;
+            var r = SQLite3.Shutdown();
+            if (r != SQLite3.Result.OK)
+                this.ErroBD = string.Format("Erro ao configurar SQLite!!! Shutdown {0}", r);
+            r = SQLite3.Config(SQLite3.ConfigOption.Serialized);
+            if (r != SQLite3.Result.OK)
+                this.ErroBD = string.Format("Erro ao configurar SQLite!!! Config {0}", r);
+            r = SQLite3.Initialize();
+            if (r != SQLite3.Result.OK)
+                this.ErroBD = string.Format("Erro ao configurar SQLite!!! Initialize {0}", r);
+
+            if (string.IsNullOrWhiteSpace(this.ErroBD))
+                Console.WriteLine("Não deu ErroBD");
+            else
+                Console.WriteLine(this.ErroBD);
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+
             SQLiteConnection conexao = new SQLiteConnection(path);
         }
     }
 }
-
